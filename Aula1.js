ca4aaebb68a3eb5d93772d5ec4498801551b9555
Bruno Dutra <brunocarioca021@gmail.com>
2021-09-21 02:48:04 +0000

    Initial commit

---
+++ Aula1.js
@@ -0,0 +1,145 @@
+// INICIO DA AULA1 TURMA C010
+
+
+// console.log("Olá Mundo");
+// console.log("Bem, vindos ao Aula  1 !!!!");
+
+//prompt("Digite o seu nome aqui: ");
+
+//JavaScript é case sensitive
+// Deve se manter boa práticas na programação.
+
+//let nomeCompleto = "Bruno Dutra 1";
+//let nomecompleto = "Bruno Dutra 2";
+
+//console.log(nomecompleto);
+
+// var nomeCompleto = prompt(" Digite o seu nome: ");
+// var endereco = prompt("Digite o seu endereço: ");
+// var turma = prompt("Digite sua turma: ");
+
+// var mensagem = "O " + nomeCompleto + " mora no endereço " + endereco + " e está ,matriculado na turma " + turma;
+
+// var mensagem2 = `O ${nomeCompleto} mora no endereço ${endereco} e está matriculado na turma ${turma} --msg2`;
+
+// console.log(mensagem);
+
+// TIPOS DE VARIÁVEIS
+
+// Texto -> String
+// let nome = "Alexandre";
+// console.log(nome);
+
+//let fraseGigante = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies vestibulum nisl, vitae pulvinar libero pulvinar ut.";
+//console.log(fraseGigante);
+
+
+//NÚMEROS 
+
+// INTEIROS
+
+// let idade = 22;
+// console.log(idade);
+
+//Decimais - Float ou Double tipos de nomes diferentes para outras linguagem de programação
+
+// let rendaMensal = 1234.56
+// console.log(rendaMensal);
+
+// typeof(rendaMensal);
+
+
+//Booleano => Verdadeiro ou Falso -> True or False
+
+//Maior que (>)
+//Menosr que (<)
+
+/// Maior ou igual que (>=)
+//Menos ou igual que (<=)
+
+// Valor igual que (==)
+// (=) Recebe , (==) Compara , (===) compara além do tipo (num,str).
+
+// let teste1 = 5 < 6;
+
+// let numero1 = 5;
+// let numero2 = "5";
+
+// let teste = numero1 ==== numero2;
+
+
+// console.log(teste);
+
+//Arrays => Listas
+
+// let listaCompras = ["Maça", "Notebook", "Celular"];
+
+// console.log(listaCompras);
+// console.log();
+// console.log();
+// console.log(listaCompras[0]);
+
+// let listaNumeros = [10, 30.8, 60, 90.5];
+
+// console.log(listaNumeros);
+// console.log();
+// console.log();
+// console.log(listaNumeros[3]);
+
+// let frase = "Helo World"
+
+// let listaMisturada = [
+//   "Maça", 
+//   `${frase} da programação`,
+//    "Notebook", 
+//    "Celular", 
+//    35.3, 
+//    false, 
+//    5==="5"
+// ];
+
+// console.log(listaMisturada);
+//console.log(listaMisturada[4]);
+
+//typeof(listaMIsturada);
+
+//OPERADORES 
+
+// let numero1 =2;
+// let numero2 = 2;
+// let somar = numero1 + numero2;
+
+
+// let soma = 1 + 2;
+// let concatenar = "1" + 2;
+
+// console.log(somar);
+// console.log(soma);
+// console.log(concatenar);
+
+//typeof(concatenar);
+
+//console.log("---------------------");
+
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(2 * 3);
+
+let multiplicacao = num1 * num2;
+console.log(multiplicacao);
+
+let subtracao = num1 - num2;
+console.log(subtracao);
+
+let divisao = num1 / num2;
+console.log(divisao);
+
+console.log(5 != "5");
+
+let idade = 15;
+console.log(idade !=15);
+
+
+
